a5bc4563ced81a45e7abb7a042ac4124fd9d9eb8
ayouya-creater <benkhaddaaya73@gmail.com>
2026-09-24 20:18:51 +0100

    suppression du 3e etudiant

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 	public static void main(String[] args) {	
 		Etudiant e1 = new Etudiant();
 		Etudiant e2 = new Etudiant();
-		Etudiant e3 = new Etudiant();
+		
 		
 		e1.nom = "AYA";
 		e1.taille = 1.64;
@@ -10,13 +10,10 @@ public class Main {
 		e2.nom ="Ayou";
 		e2.taille = 1.74;
 		e2.age = 31;
-		e3.nom = "Sara";
-		e3.taille = 1.68;
-		e3.age = 25;
 				
 		e1.afficher();
 		e2.afficher();
-		e3.afficher();
+	
 		
 		Voiture v1 = new Voiture();
 		v1.marque = "Toyota";
